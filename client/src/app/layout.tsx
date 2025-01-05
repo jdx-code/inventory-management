c8a948b5f0316db1ca8067f55902f66e0ae606c9
Jyotirmoy <jyotirmoydas12@gmail.com>
2025-01-05 19:38:34 +0530

    commenting imports on layout

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono, Inter } from "next/font/google";
+// import { Geist, Geist_Mono } from "next/font/google";
+import { Inter } from "next/font/google";
 import "./globals.css";
 import DashboardWrapper from "./dashboardWrapper";
 
