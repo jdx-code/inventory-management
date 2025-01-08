e3ca146aac31448efeb316f1ea9a3ed4ae169cfe
Jyotirmoy <jyotirmoydas12@gmail.com>
2025-01-09 00:08:28 +0530

    added images

--- client/src/app/(components)/Sidebar/index.tsx
+++ client/src/app/(components)/Sidebar/index.tsx
@@ -3,6 +3,7 @@
 import { useAppDispatch, useAppSelector } from '@/app/redux';
 import { setIsSidebarCollapsed } from '@/state';
 import { Archive, CircleDollarSign, Clipboard, Layout, LucideIcon, Menu, SlidersHorizontal, User } from 'lucide-react';
+import Image from 'next/image';
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
 import React from 'react';
@@ -63,7 +64,13 @@ const Sidebar = () => {
     <div className={sidebarClassNames}>
         {/* TOP LOGO */}
         <div className={`flex gap-3 justify-between md:justify-normal items-center pt-8 ${isSidebarCollapsed ? "px-5" : "px-8"}`}>
-            <div>logo</div>
+            <Image 
+                src="https://s3-jd-inventorymanagement.s3.ap-south-1.amazonaws.com/logo.png"
+                alt="zstock-logo"
+                width={27}
+                height={27}
+                className="rounded w-8"
+            />
             <h1 className={`${isSidebarCollapsed ? "hidden" : "block"} font-extrabold text-lg`}>Z-STOCK</h1>
 
             <button 
